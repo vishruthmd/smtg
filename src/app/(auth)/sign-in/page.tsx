a54cf530c13daed589fd3dcbb9f256e75a189efa
vishruthmd <vishruthmd18@gmail.com>
2025-06-24 23:56:50 +0530

    sign in and sign up auth ui

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,10 @@
+import { Card } from "@/components/ui/card";
+import { SignInView } from "@/modules/auth/ui/views/sign-in-view";
+import React from "react";
+
+const Page = () => {
+    console.log("sign in page");
+    return <SignInView />;
+};
+
+export default Page;
---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,8 @@
+import { SignUpView } from "@/modules/auth/ui/views/sign-up-view";
+
+const Page = () => {
+    console.log("sign up page");
+    return <SignUpView />;
+};
+
+export default Page;
