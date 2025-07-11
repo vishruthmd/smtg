206fdfee3070b05292e6f6f563194621a93888a8
vishruthmd <vishruthmd18@gmail.com>
2025-07-12 01:54:12 +0530

    meeting page

---
+++ src/modules/meetings/ui/views/meeting-id-view.tsx
@@ -0,0 +1,95 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import {
+    useMutation,
+    useQueryClient,
+    useSuspenseQuery,
+} from "@tanstack/react-query";
+
+import { ErrorState } from "@/components/error-state";
+import { LoadingState } from "@/components/loading-state";
+import { useTRPC } from "@/trpc/client";
+import { useConfirm } from "@/hooks/use-confirm";
+
+import { MeetingIdViewHeader } from "../components/meeting-id-view-header";
+import { UpdateMeetingDialog } from "../components/update-meeting-dialog";
+import { useState } from "react";
+
+interface Props {
+    meetingId: string;
+}
+
+export const MeetingIdView = ({ meetingId }: Props) => {
+    const trpc = useTRPC();
+    const router = useRouter();
+    const queryClient = useQueryClient();
+    const [updateMeetingDialogOpen, setUpdateMeetingDialogOpen] =
+        useState(false);
+
+    const [RemoveConfirmation, confirmRemove] = useConfirm(
+        "Are you sure?",
+        "The following action will remove this meeting"
+    );
+
+    const { data } = useSuspenseQuery(
+        trpc.meetings.getOne.queryOptions({ id: meetingId })
+    );
+
+    const removeMeeting = useMutation(
+        trpc.meetings.remove.mutationOptions({
+            onSuccess: () => {
+                queryClient.invalidateQueries(
+                    trpc.meetings.getMany.queryOptions({})
+                );
+                router.push("/meetings");
+                // invalidate free tier usage here as well
+            },
+        })
+    );
+
+    const handleRemoveMeeting = async () => {
+        const ok = await confirmRemove();
+        if (!ok) return;
+
+        await removeMeeting.mutateAsync({ id: meetingId });
+    };
+
+    return (
+        <>
+            <RemoveConfirmation />
+            <UpdateMeetingDialog
+                open={updateMeetingDialogOpen}
+                onOpenChange={setUpdateMeetingDialogOpen}
+                initialValues={data}
+            />
+            <div className="flex-1 py-4 px-4 md:px-8 flex flex-col gap-y-4">
+                <MeetingIdViewHeader
+                    meetingId={meetingId}
+                    meetingName={data.name}
+                    onEdit={() => setUpdateMeetingDialogOpen(true)}
+                    onRemove={handleRemoveMeeting}
+                />
+                {JSON.stringify(data, null, 2)}
+            </div>
+        </>
+    );
+};
+
+export const MeetingIdViewLoading = () => {
+    return (
+        <LoadingState
+            title="Loading your meeting"
+            description="This may take a few seconds"
+        />
+    );
+};
+
+export const MeetingIdViewError = () => {
+    return (
+        <ErrorState
+            title="Error loading your meeting"
+            description="Something went wrong"
+        />
+    );
+};
